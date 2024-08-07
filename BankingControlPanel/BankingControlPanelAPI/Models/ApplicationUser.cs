de8805506e33678b86535b2e76fe62a2d36e3b8c
Zain Arif <zainarif14197@gmail.com>
2024-08-07 08:43:20 +0500

    ASP.NET core identity setup

---
+++ BankingControlPanel/BankingControlPanelAPI/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BankingControlPanelAPI.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+    }
+}
--- BankingControlPanel/BankingControlPanelAPI/Program.cs
+++ BankingControlPanel/BankingControlPanelAPI/Program.cs
@@ -1,6 +1,20 @@
+using BankingControlPanelAPI.Data;
+using BankingControlPanelAPI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -22,4 +36,19 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+ApplyMigration();
+
 app.Run();
+
+void ApplyMigration()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        if (_db.Database.GetPendingMigrations().Count() > 0)
+        {
+            _db.Database.Migrate();
+        }
+    }
+}
