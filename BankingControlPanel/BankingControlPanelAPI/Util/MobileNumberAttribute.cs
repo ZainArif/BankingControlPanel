b3d78646f079b95d7358f786ad82cfde73dfcd74
Zain Arif <zainarif14197@gmail.com>
2024-08-10 14:54:02 +0500

    mobile number validation attribute

---
+++ BankingControlPanel/BankingControlPanelAPI/Util/MobileNumberAttribute.cs
@@ -0,0 +1,54 @@
+﻿using PhoneNumbers;
+using System.ComponentModel.DataAnnotations;
+
+namespace BankingControlPanelAPI.Util
+{
+    public class MobileNumberAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            try
+            {
+                var phoneNumberUtil = PhoneNumberUtil.GetInstance();
+
+                var parsedPhoneNumber = phoneNumberUtil.Parse(value.ToString(), null);
+
+                var isValidNumber = phoneNumberUtil.IsValidNumber(parsedPhoneNumber);
+
+                if (!isValidNumber)
+                    return new ValidationResult(GetErrorMessage());
+
+                return ValidationResult.Success;
+            }
+            catch (NumberParseException npex)
+            {
+                Console.WriteLine(npex);
+
+                return new ValidationResult(GetErrorMessage(npex.ErrorType.ToString()));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+
+                return new ValidationResult(GetErrorMessage());
+            }
+        }
+
+        private string GetErrorMessage(string errorCode = "")
+        {
+            string message = string.Empty;
+            
+            switch (errorCode)
+            {
+                case "INVALID_COUNTRY_CODE":
+                    message = "Phone number must be with valid country code.";
+                    break;
+                default:
+                    message = "Invalid phone number."; 
+                    break;
+            }
+
+            return message;
+        }
+    }
+}
